46f5e9a9052a338adf9bba26a1ff1fa3d31fb6cf
Tushar Paliwal <tushar.paliwal1997@gmail.com>
2018-10-15 00:34:55 +0530

    Update MainActivity.java

--- app/src/main/java/com/gdgvitvellore/pricecalculator/MainActivity.java
+++ app/src/main/java/com/gdgvitvellore/pricecalculator/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+// Improved by vulfpeck on 15/10/18
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
